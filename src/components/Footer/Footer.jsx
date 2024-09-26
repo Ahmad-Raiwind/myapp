a196ed8ebca66380b572a635f349d57310dea6b5
Ahmad-Raiwind <raiwind.pkistan@gmail.com>
2024-09-26 13:25:11 +0500

    footer commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <footer>
         <ul>
             <li><a href="#">Facebook</a></li>
-            <li><a href="#">Instagram</a></li>
+            <li><a href="#">Instagram good</a></li>
             <li><a href="#">Twitter</a></li>
         </ul>
         <div className="user-content">
